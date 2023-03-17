de268758aa4fc175dd4c2bf7dbafa6e2ebcfe3ec
won <wonjae124@naver.com>
2023-03-17 18:15:54 +0900

    Fix sayHello() to return the correct value

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
-	msg := sayHello("Allice")
+func main() {
+	msg := sayHello("Alice")
 	fmt.Println(msg)
 }
 
 func sayHello(name string) string {
-	return fmt.Sprintf("Hi %s", name)
+	// Change this to "Hello %s" instead of "Hi %s".
+	return fmt.Sprintf("Hello %s", name)
 }
